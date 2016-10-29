f826eee1613a43d23dc2403f297e73b2423115a0
Jae Kim <jaekimjs@gmail.com>
2016-10-29 02:32:16 -0700

    gave main calendar its own route

--- src/app/app.js
+++ src/app/app.js
@@ -3,9 +3,16 @@
             $logProvider.debugEnabled(false);
             $compileProvider.debugInfoEnabled(false);
 
+            $stateProvider.state('calendar',{
+                url:'/',
+                templateUrl:'./src/app/calendar/calendar.html'
+            })
+
             $stateProvider.state('events',{
                 url:'/events/:month/:day',
                 templateUrl:'./src/app/calendar/events/eventsManager.html'
-            })
+            });
+
+            $urlRouterProvider.otherwise('/');
         }]);      
 })(window,window.angular);
