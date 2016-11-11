c782788c2cd6a0076bea79f7ecc8abbccef9ac0c
Jae Kim <jaekimjs@gmail.com>
2016-11-10 22:04:03 -0800

    fixed retarded test

--- src/app/calendar/calendarBuilder/spec/calendarBuilder.spec.js
+++ src/app/calendar/calendarBuilder/spec/calendarBuilder.spec.js
@@ -18,16 +18,16 @@ describe('calendarBuilder methods',function(){
 
     describe('createWeeks',function(){
         describe('should build the weeks arrays using the createWeeks factory method',function(){
-            it('generated weeks\' first day in days array of first week should equal calendar\'s first day',function(){                  
-                expect(generatedWeeksOfMonth.weeks[0].days[0].moment.date() == startOfCalendarViewMoment.date())
+            it('generated weeks\' first day in days array of first week should equal calendar\'s first day',function(){
+                expect(generatedWeeksOfMonth.weeks[0].days[0].moment.date()).toEqual(startOfCalendarViewMoment.date())
             })
 
             it('generated weeks\' last day in days array of last week should equal calendar\'s last day',function(){
                 weeksArray = generatedWeeksOfMonth.weeks;
                 daysArray = weeksArray[weeksArray.length-1].days;
-                expect(daysArray[daysArray.length-1].moment.date() == startOfCalendarViewMoment.endOf('month').day(6).date());
+                expect(daysArray[daysArray.length-1].moment.date()).toEqual(startOfCalendarViewMoment.day(6).endOf('month').day(6).date());
                 startOfCalendarViewMoment.date(1).day(0);
             })
         })
-    }); 
+    });
 });
